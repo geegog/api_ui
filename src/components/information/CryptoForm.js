96110b89ed8c8123b7002ac05ed84c13dd7b11c7
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-07-02 23:22:32 +0300

    final push

--- src/components/information/CryptoForm.js
+++ src/components/information/CryptoForm.js
@@ -10,12 +10,29 @@ export default class CryptoForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
+            value: '',
+            editeMode: false,
+            links: {}
         }
 
         this.authObj = new auth();
         this.handleValueChange = this.handleValueChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    componentDidUpdate(prevProps) {
+        
+        const { action } = prevProps;
+        //console.log(action, this.props.action.value);
+        const cValue = this.props.action.value;
+        const links = this.props.action.links;
+        if (action.value !== cValue) {
+            this.setState({ value: cValue });
+            this.setState({ links: links })
+            this.setState({ editeMode: true })
+        }
+        
     }
 
     handleValueChange = event => {
@@ -25,18 +42,76 @@ export default class CryptoForm extends React.Component {
     handleSubmit = event => {
         event.preventDefault();
 
+        let url = '';
+
         const value = {
-            value: this.state.value,
+            value: this.state.value
         };
+
+        if (this.state.editeMode) {
+            url = this.state.links.encrypt_update.href;
+        } else {
+            url = 'http://localhost:8080/api/data/encrypt';
+        }
 
         axios({
             method: 'post',
-            url: 'http://localhost:8080/api/data/encrypt',
+            url: url,
             headers: this.authObj.getAuthHeader(),
             data: value
         })
             .then(res => {
                 cogoToast.success("Data encrypted successfully!");
+
+                this.props.handleStateChange(res.data);
+                if (!this.state.editeMode) {
+                    this.setState({ value: '' });
+                }
+
+                // console.log(res);
+                // console.log(res.data);
+            })
+            .catch((error) => {
+                if (error.response) {
+                    cogoToast.error(error.response.data.message);
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
+                    // console.log(error.response.headers);
+                } else if (error.request) {
+                    cogoToast.error("Network error!");
+                    //console.log(error);
+                } else {
+                    cogoToast.error(error.message);
+                    //console.log('Error', error.message);
+                }
+            })
+    }
+
+    handleDecrypt = event => {
+        event.preventDefault();
+
+        const value = {
+            value: this.state.value
+        };
+
+        axios({
+            method: 'post',
+            url: this.state.links.decrypt.href,
+            headers: this.authObj.getAuthHeader(),
+            data: value
+        })
+            .then(res => {
+                cogoToast.success("Data decrypted successfully!");
+
+                cogoToast.info(res.data.value, {
+                    onClick: hide => {
+                        hide();
+                    },
+                    hideAfter: 30,
+                    heading: 'Here is the real content :)',
+                    bar: {size: '4px', style: 'dotted'},
+                });
+
                 // console.log(res);
                 // console.log(res.data);
             })
@@ -57,6 +132,7 @@ export default class CryptoForm extends React.Component {
     }
 
     render() {
+        console.log(this.state.editeMode);
         return (
             <div className="crypto-form">
                 <Form onSubmit={this.handleSubmit}>
@@ -66,7 +142,7 @@ export default class CryptoForm extends React.Component {
                         <Input required type="text" name="value" id="value" placeholder="value" value={this.state.value} onChange={this.handleValueChange} />
                     </FormGroup>
                     <div className="btn-group float-right">
-                        <Button color="warning" className="btn btn-lg" type="submit" >Decrypt</Button>
+                        { this.state.editeMode === false ? null : <Button color="warning" className="btn btn-lg" onClick={this.handleDecrypt}>Decrypt</Button> }
                         <Button color="primary" className="btn btn-lg" type="submit">Encrypt</Button>
                     </div>
                 </Form>
--- src/components/information/Profile.js
+++ src/components/information/Profile.js
@@ -1,26 +1,91 @@
+import { Table, Button } from 'reactstrap';
 import React from 'react';
 import Crypto from './CryptoForm'
 import auth from '../auth/auth'
-import DataList from './DataList'
+import axios from 'axios';
 
 export default class RegisterForm extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
-    
+        this.state = {
+            datas: [],
+            action: {
+                value: '',
+                links: {}
+            }
+        }
+
         this.authObj = new auth();
         console.log(this.authObj.isAthenticated());
         if (!this.authObj.isAthenticated()) {
-          this.props.history.push("/");
+            this.props.history.push("/");
         }
-      }
+
+        this.handleStateChange = this.handleStateChange.bind(this);
+        this.setValue = this.setValue.bind(this);
+    }
+
+    handleStateChange(value) {
+        const index = this.state.datas.findIndex((record) => { return record._id === value._id });
+
+        console.log('index: ', index);
+
+        if (index >= 0) {
+            const newDatas = Object.assign([], this.state.datas);
+            newDatas[index] = value;
+            this.setState({ datas: newDatas });
+        } else {
+            this.setState(prevState => ({
+                datas: [...prevState.datas, value]
+            }))
+        }
+
+    }
+
+    setValue(value, links) {
+        this.setState({ action: { value, links } })
+    }
+
+    componentDidMount() {
+        axios({
+            method: 'get',
+            url: 'http://localhost:8080/api/data/' + this.authObj.authUser().sub + '/records',
+            headers: this.authObj.getAuthHeader()
+        })
+            .then(res => {
+                console.log(res.data)
+                const datas = res.data._embedded !== undefined ? res.data._embedded.recordDTOList : [];
+                this.setState({ datas: datas });
+            })
+    }
 
     render() {
         return (
             <div className="profile">
-                <Crypto />
-                <DataList />
+                <Crypto handleStateChange={this.handleStateChange} action={this.state.action} />
+                <div className="table-responsive">
+                    <Table striped>
+                        <thead>
+                            <tr>
+                                <th>Value</th>
+                                <th>Created</th>
+                                <th>Updated</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.datas.map((data) => <tr key={data._id}>
+                                    <td className="d-inline-block text-truncate wrap-width">{data.value}</td>
+                                    <td>{data.created}</td>
+                                    <td>{data.updated}</td>
+                                    <td><Button onClick={() => this.setValue(data.value, data._links)}>Select</Button></td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         )
     }
